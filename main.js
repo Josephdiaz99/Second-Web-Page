50dcd37a7ffd04e5e72aa4101bf51e8f4d893cf6
Josephdiaz99 <100252077+Josephdiaz99@users.noreply.github.com>
2022-02-23 15:32:29 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+document.querySelector('.menu-btn').addEventListener('click',()=>{
+ document.querySelector('.nav-menu').classList.toggle('show');
+});
+
+
+
+
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.news-cards', {delay: 500}) ;
+ScrollReveal().reveal('.banner-one', {delay: 500}) ;
+ScrollReveal().reveal('.banner-two', {delay: 500}) ;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
